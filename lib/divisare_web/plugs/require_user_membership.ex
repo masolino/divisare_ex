407283603aaaf90cbf62162365a6e267b0aa7b5a
Andrea Pavoni <andrea.pavoni@gmail.com>
2024-07-09 10:21:10 +0200

    fix checking user membership

--- lib/divisare_web/plugs/require_user_membership.ex
+++ lib/divisare_web/plugs/require_user_membership.ex
@@ -7,7 +7,6 @@ defmodule DivisareWeb.Plugs.RequireUserMembership do
       plug DivisareWeb.Plugs.RequireUserMembership
   """
 
-  import Plug.Conn
   alias Divisare.Subscriptions
   alias Divisare.Accounts
   import Phoenix.Controller
@@ -17,17 +16,11 @@ defmodule DivisareWeb.Plugs.RequireUserMembership do
   def call(conn, _opts) do
     with {:ok, user} <- Accounts.find_user_by_token(conn.params["token"]) do
       case Subscriptions.guess_user_enrollment(user) do
-        {:subscription, sub} ->
-          {:subscription, sub}
-
-        {:team, team} ->
-          {:team, team}
-
-        {:board, board} ->
-          {:board, board}
-
         {:error, _} ->
           redirect(conn, external: "#{Application.get_env(:divisare, :main_host)}/subscriptions")
+
+        _ ->
+          conn
       end
     else
       _ -> redirect(conn, external: "#{Application.get_env(:divisare, :main_host)}")
