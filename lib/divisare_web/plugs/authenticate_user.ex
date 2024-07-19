b2a0811b9dd61fc0439ef97627952268bff38f0c
Andrea Pavoni <andrea.pavoni@gmail.com>
2024-07-19 12:26:15 +0200

    fix error in user authentication plug

--- lib/divisare_web/plugs/authenticate_user.ex
+++ lib/divisare_web/plugs/authenticate_user.ex
@@ -18,7 +18,7 @@ defmodule DivisareWeb.Plugs.AuthenticateUser do
   """
   def call(conn, _opts) do
     with user_id when not is_nil(user_id) <-
-           get_in(conn, [Access.key!(:assigns), Access.key!(:current_user_id)]),
+           get_in(conn, [Access.key(:assigns), Access.key(:current_user_id)]),
          {:ok, user} <- Accounts.find_user_by_id(user_id) do
       assign(conn, :current_user, user)
     else
